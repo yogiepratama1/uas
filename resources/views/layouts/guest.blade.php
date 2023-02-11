5061d119614b7a08e48a26a95440610a2ceae912
unknown <pratamayogie131@gmail.com>
2023-02-11 17:29:31 +0700

    2

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,8 @@
     <style>
     </style>
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('app.css') }}" />
+
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
